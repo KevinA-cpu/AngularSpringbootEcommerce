684cabbf374f47c97f022f087669cf541c421fe0
Heisenburg <kevinanderson1410@gmail.com>
2024-07-17 21:48:48 +0700

    feat: secure backend

---
+++ spring-boot-ecommerce/src/main/java/com/heisenburg/springbootecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.heisenburg.springbootecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/orders/**").authenticated()
+                                .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                ).cors(Customizer.withDefaults());
+
+        httpSecurity.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+        Okta.configureResourceServer401ResponseBody(httpSecurity);
+
+        return httpSecurity.build();
+    }
+
+
+}
